48fd6ed16133972c2381f467b4667ccdc586b52d
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2025-08-27 09:47:35 -0400

    exclude opened and non-merged PRs

--- generate_release_notes.py
+++ generate_release_notes.py
@@ -346,6 +346,8 @@ def release_notes(parsed_args):
                 if prr_list:
                     for pr in prr_list:
                         try:
+                            if pr.state != "closed" or not pr.merged:
+                                continue
                             jira_keys, pr_github_issues = get_issues_from_pr(repo, pr.number)
                             if jira_keys:
                                 for jira_key in jira_keys:
